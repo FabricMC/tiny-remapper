98347db52e24b2f1b4e893bd4d294c8548c9a28f
Adrian Siekierka <kontakt@asie.pl>
2018-10-27 15:58:19 +0200

    fix Opcodes include, update license year

--- src/main/java/net/fabricmc/tinyremapper/AsmClassRemapper.java
+++ src/main/java/net/fabricmc/tinyremapper/AsmClassRemapper.java
@@ -1,14 +1,30 @@
+/*
+ * Copyright (C) 2016, 2018 Player, asie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.fabricmc.tinyremapper;
 
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.ClassRemapper;
 import org.objectweb.asm.commons.MethodRemapper;
 import org.objectweb.asm.commons.Remapper;
-
-import jdk.internal.org.objectweb.asm.Opcodes;
 
 class AsmClassRemapper extends ClassRemapper {
 	public AsmClassRemapper(ClassVisitor cv, AsmRemapper remapper) {
