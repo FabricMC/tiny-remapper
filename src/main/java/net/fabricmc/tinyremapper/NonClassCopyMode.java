20dd6c65e0b211874255523585fedd1d5b4fd43c
Player <player@player.to>
2019-04-05 17:41:32 +0200

    Disable license gradle plugin until it can be fixed

--- src/main/java/net/fabricmc/tinyremapper/NonClassCopyMode.java
+++ src/main/java/net/fabricmc/tinyremapper/NonClassCopyMode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019, Player
+ * Copyright (C) 2016, 2018 Player, asie
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
