a03a36293a876bf0b8e0756c2b6a1d14c7eddb1f
modmuss50 <modmuss50@gmail.com>
2020-12-29 00:04:58 +0000

    Update build script + release via github actions. Bump ASM to 9.0

--- src/main/java/net/fabricmc/tinyremapper/TinyRemapper.java
+++ src/main/java/net/fabricmc/tinyremapper/TinyRemapper.java
@@ -426,7 +426,7 @@ public class TinyRemapper {
 
 		final ClassInstance ret = new ClassInstance(this, isInput, tags, srcPath, isInput ? data : null);
 
-		reader.accept(new ClassVisitor(Opcodes.ASM8, extraAnalyzeVisitor) {
+		reader.accept(new ClassVisitor(Opcodes.ASM9, extraAnalyzeVisitor) {
 			@Override
 			public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
 				ret.init(name, superName, access, interfaces);
@@ -841,7 +841,7 @@ public class TinyRemapper {
 		ClassVisitor visitor = writer;
 
 		if (rebuildSourceFilenames) {
-			visitor = new SourceNameRebuildVisitor(Opcodes.ASM8, visitor);
+			visitor = new SourceNameRebuildVisitor(Opcodes.ASM9, visitor);
 		}
 
 		if (check) {
@@ -886,7 +886,7 @@ public class TinyRemapper {
 		ClassReader reader = new ClassReader(data);
 		ClassWriter writer = new ClassWriter(0);
 
-		reader.accept(new ClassVisitor(Opcodes.ASM8, writer) {
+		reader.accept(new ClassVisitor(Opcodes.ASM9, writer) {
 			@Override
 			public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
 				if (makeClsPublic) {
