41c3b8ba34a46d69439e78ca1fb76b570335ecf4
LogicFan <38597904+LogicFan@users.noreply.github.com>
2021-05-26 02:35:56 -0400

    add test for tiny remapper

---
+++ src/test/java/net/fabricmc/tinyremapper/TestUtil.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016, 2018 Player, asie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.fabricmc.tinyremapper;
+
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.nio.file.Path;
+
+public class TestUtil {
+    public static File copyFile(Class cls, TemporaryFolder folder, String path)
+            throws IOException {
+        try (InputStream input = cls.getResourceAsStream(path)) {
+            byte[] buffer = new byte[input.available()];
+            input.read(buffer);
+
+            File target = folder.newFile(path.replace('/', '_'));
+
+            try (OutputStream output = new FileOutputStream(target)) {
+                output.write(buffer);
+            }
+
+            return target;
+        }
+    }
+}
