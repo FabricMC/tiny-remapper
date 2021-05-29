66d0eac41d9e6888386ee543f14626bc23f945e0
Devan-Kerman <dev.sel20@gmail.com>
2021-05-29 10:56:08 -0500

    lazy rename fix

--- src/main/java/net/fabricmc/tinyremapper/util/Lazy.java
+++ src/main/java/net/fabricmc/tinyremapper/util/Lazy.java
@@ -5,29 +5,29 @@ import java.util.concurrent.Callable;
 import java.util.function.Supplier;
 
 public final class Lazy<T> implements Supplier<T> {
-	private Callable<T> supplier;
+	private Callable<T> callable;
 	private T instance;
 
-	public Lazy(Callable<T> supplier) {
-		this.supplier = Objects.requireNonNull(supplier, "Supplier may not be null");
+	public Lazy(Callable<T> callable) {
+		this.callable = Objects.requireNonNull(callable, "Callable may not be null");
 	}
 
 	@Override
 	public T get() {
 		T instance = this.instance;
-		Callable<T> supplier = this.supplier;
+		Callable<T> supplier = this.callable;
 		if(supplier != null) {
 			try {
 				this.instance = supplier.call();
 			} catch (Exception exception) {
 				throw new RuntimeException(exception);
 			}
-			this.supplier = null;
+			this.callable = null;
 		}
 		return instance;
 	}
 
 	public boolean hasEvaluated() {
-		return this.supplier == null;
+		return this.callable == null;
 	}
 }
