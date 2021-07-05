8b1b8dcc0393d92d1e5cd0b3ce9aed8404f6ad0a
Devan-Kerman <dev.sel20@gmail.com>
2021-07-05 17:44:48 -0500

    Update WrapperFunction.java

--- src/main/java/net/fabricmc/tinyremapper/api/WrapperFunction.java
+++ src/main/java/net/fabricmc/tinyremapper/api/WrapperFunction.java
@@ -5,6 +5,7 @@ import org.objectweb.asm.ClassVisitor;
 /**
  * @see ClassVisitor#ClassVisitor(int, ClassVisitor)
  */
+@FunctionalInterface
 public interface WrapperFunction {
 	ClassVisitor wrap(ClassVisitor visitor, ExtendedRemapper remapper, Classpath classpath);
 }
