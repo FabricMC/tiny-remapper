2e5cbf850ad45dfc5e38b87cdac473507809ed56
LogicFan <38597904+LogicFan@users.noreply.github.com>
2021-07-25 11:08:22 -0400

    refactor HardTargetMappable

--- src/main/java/net/fabricmc/tinyremapper/extension/mixin/common/data/CommonData.java
+++ src/main/java/net/fabricmc/tinyremapper/extension/mixin/common/data/CommonData.java
@@ -5,15 +5,20 @@ import java.util.Objects;
 import net.fabricmc.tinyremapper.api.TrEnvironment;
 import net.fabricmc.tinyremapper.api.TrRemapper;
 import net.fabricmc.tinyremapper.extension.mixin.common.Logger;
+import net.fabricmc.tinyremapper.extension.mixin.common.ResolveUtility;
 
 public final class CommonData {
 	public final TrRemapper remapper;
 	public final TrEnvironment environment;
 	public final Logger logger;
 
+	public final ResolveUtility resolver;
+
 	public CommonData(TrEnvironment environment, Logger logger) {
 		this.remapper = environment.getRemapper();
 		this.environment = Objects.requireNonNull(environment);
 		this.logger = Objects.requireNonNull(logger);
+
+		this.resolver = new ResolveUtility(environment, logger);
 	}
 }
--- src/main/java/net/fabricmc/tinyremapper/extension/mixin/hard/util/HardTargetMappable.java
+++ src/main/java/net/fabricmc/tinyremapper/extension/mixin/hard/util/HardTargetMappable.java
@@ -3,26 +3,24 @@ package net.fabricmc.tinyremapper.extension.mixin.hard.util;
 import java.util.Objects;
 import java.util.Optional;
 
-import net.fabricmc.tinyremapper.api.TrMember;
 import net.fabricmc.tinyremapper.extension.mixin.common.IMappable;
-import net.fabricmc.tinyremapper.extension.mixin.common.ResolveUtility;
 import net.fabricmc.tinyremapper.extension.mixin.common.data.CommonData;
 import net.fabricmc.tinyremapper.extension.mixin.common.data.MxMember;
 
 public abstract class HardTargetMappable implements IMappable<Void> {
 	protected CommonData data;
-	protected TrMember self;
+	protected MxMember self;
 
 	public HardTargetMappable(CommonData data, MxMember self) {
 		this.data = Objects.requireNonNull(data);
-		this.self = Objects.requireNonNull(self).asTrMember(new ResolveUtility(data.environment, data.logger));
+		this.self = Objects.requireNonNull(self);
 	}
 
 	protected abstract Optional<String> getMappedName();
 
 	@Override
 	public Void result() {
-		getMappedName().ifPresent(x -> data.environment.propagate(self, x));
+		getMappedName().ifPresent(x -> data.environment.propagate(self.asTrMember(data.resolver), x));
 		return null;
 	}
 }
