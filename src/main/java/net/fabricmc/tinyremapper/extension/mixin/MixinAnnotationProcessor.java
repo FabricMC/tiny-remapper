6cb1083f02e2b1cd5160696835d751160cc619c7
LogicFan <38597904+LogicFan@users.noreply.github.com>
2021-07-27 23:07:43 -0400

    modify based on sfPlayers commit

--- src/main/java/net/fabricmc/tinyremapper/extension/mixin/MixinAnnotationProcessor.java
+++ src/main/java/net/fabricmc/tinyremapper/extension/mixin/MixinAnnotationProcessor.java
@@ -1,11 +1,17 @@
 package net.fabricmc.tinyremapper.extension.mixin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 import org.objectweb.asm.ClassVisitor;
 
+import net.fabricmc.tinyremapper.TinyRemapper.AnalyzeVisitorProvider;
+import net.fabricmc.tinyremapper.TinyRemapper.ApplyVisitorProvider;
+import net.fabricmc.tinyremapper.TinyRemapper.StateProcessor;
+import net.fabricmc.tinyremapper.api.TrClass;
 import net.fabricmc.tinyremapper.api.TrEnvironment;
 import net.fabricmc.tinyremapper.extension.mixin.common.Logger;
 import net.fabricmc.tinyremapper.extension.mixin.common.data.CommonData;
@@ -17,31 +23,30 @@ import net.fabricmc.tinyremapper.extension.mixin.soft.SoftTargetMixinClassVisito
  * <p>Soft-target: Mixin, Invoker, Accessor, Inject, ModifyArg, ModifyArgs, Redirect, ModifyVariable, ModifyConstant, At, Slice.</p>
  * <p>Hard-target: Shadow, Overwrite, Accessor, Invoker, Implements.</p>
  */
-public class MixinAnnotationProcessor {
+public class MixinAnnotationProcessor implements AnalyzeVisitorProvider, ApplyVisitorProvider, StateProcessor {
 	private final Logger logger;
-	private final List<Consumer<CommonData>> tasks;
+	private final Map<Integer, List<Consumer<CommonData>>> tasks;
 
 	public MixinAnnotationProcessor() {
 		this.logger = new Logger();
-		this.tasks = new ArrayList<>();
+		this.tasks = new HashMap<>();
 	}
 
 	public MixinAnnotationProcessor(Logger.Level logLevel) {
 		this.logger = new Logger(logLevel);
-		this.tasks = new ArrayList<>();
+		this.tasks = new HashMap<>();
 	}
 
-	public ClassVisitor getAnalyzeVisitor(ClassVisitor cv) {
-		return new HardTargetMixinClassVisitor(tasks, cv);
+	@Override
+	public ClassVisitor insertAnalyzeVisitor(int mrjVersion, String className, ClassVisitor next) {
+		tasks.putIfAbsent(mrjVersion, new ArrayList<>());
+		return new HardTargetMixinClassVisitor(tasks.get(mrjVersion), next);
 	}
 
-	public ClassVisitor getAnalyzeVisitor() {
-		return getAnalyzeVisitor(null);
-	}
-
+	@Override
 	public void process(TrEnvironment environment) {
 		CommonData data = new CommonData(environment, logger);
-		tasks.forEach(task -> {
+		tasks.get(environment.getMrjVersion()).forEach(task -> {
 			try {
 				task.accept(data);
 			} catch (RuntimeException e) {
@@ -50,8 +55,9 @@ public class MixinAnnotationProcessor {
 		});
 	}
 
-	public ClassVisitor getPreApplyVisitor(ClassVisitor cv, TrEnvironment environment) {
-		return new SoftTargetMixinClassVisitor(new CommonData(environment, logger), cv);
+	@Override
+	public ClassVisitor insertApplyVisitor(TrClass cls, ClassVisitor next) {
+		return new SoftTargetMixinClassVisitor(new CommonData(cls.getEnvironment(), logger), next);
 	}
 }
 
