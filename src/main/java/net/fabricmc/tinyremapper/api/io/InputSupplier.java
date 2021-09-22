5442df3a4fb82d55d162cfda8d79dd158784ec4c
Logic <38597904+LogicFan@users.noreply.github.com>
2021-09-21 21:26:06 -0400

    add open & close method

--- src/main/java/net/fabricmc/tinyremapper/api/io/InputSupplier.java
+++ src/main/java/net/fabricmc/tinyremapper/api/io/InputSupplier.java
@@ -2,12 +2,30 @@ package net.fabricmc.tinyremapper.api.io;
 
 import net.fabricmc.tinyremapper.api.TrClass;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.nio.file.Path;
 
 public interface InputSupplier {
+    /**
+     * Get the source of the input. Only useful for logging.
+     */
     String getSource();
+
+    /**
+     * Must be called exactly once before {@link InputSupplier#load(InputConsumer)}.
+     */
+    void open() throws IOException;
+
+    /**
+     * Load files into {@link InputConsumer}. Only valid after {@link InputSupplier#open()}, before {@link InputSupplier#close()}.
+     */
     void load(InputConsumer consumer) throws IOException;
+
+    /**
+     * Release any resources bind with this object.
+     */
+    void close() throws IOException;
 
     @FunctionalInterface
     interface DataSupplier {
