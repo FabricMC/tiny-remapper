3e88456677252b74da843bad1a0e6f4a182c4c2c
calmilamsy <bumbill00@gmail.com>
2022-02-24 16:41:24 +0000

    Ignore empty values

--- src/main/java/net/fabricmc/tinyremapper/TinyUtils.java
+++ src/main/java/net/fabricmc/tinyremapper/TinyUtils.java
@@ -171,8 +171,10 @@ public final class TinyUtils {
 				tmp,
 				(remapClasses, classMapper) -> {
 					for (Member m : members) {
-						if (remapClasses) m.owner = classMapper.map(m.owner);
-						m.desc = classMapper.mapDesc(m.desc);
+						if (!m.owner.isEmpty()) {
+							if (remapClasses) m.owner = classMapper.map(m.owner);
+							m.desc = classMapper.mapDesc(m.desc);
+						}
 					}
 				});
 
