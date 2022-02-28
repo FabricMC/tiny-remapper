8a48c0726b162f2380e60b35b6ad4ac3c260279f
calmilamsy <bumbill00@gmail.com>
2022-02-28 17:07:15 +0000

    Update copyright date

--- src/main/java/net/fabricmc/tinyremapper/TinyUtils.java
+++ src/main/java/net/fabricmc/tinyremapper/TinyUtils.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016, 2018, Player, asie
- * Copyright (c) 2016, 2021, FabricMC
+ * Copyright (c) 2016, 2022, FabricMC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
