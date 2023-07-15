2b857a687cd242b40f195ef0201e2e26a43a9350
Luke Bemish <lukebemish@lukebemish.dev>
2023-07-14 23:40:44 -0400

    Fix checkstyle violations

--- src/main/java/net/fabricmc/tinyremapper/AsmClassRemapper.java
+++ src/main/java/net/fabricmc/tinyremapper/AsmClassRemapper.java
@@ -26,8 +26,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.lang.model.SourceVersion;
 
