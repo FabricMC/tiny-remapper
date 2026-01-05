036a1b831e654cd2cdf41362bb3280037f801c2f
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-01-05 21:54:59 +0100

    fix: resolve typo in message
    there was a typo in the message when a mapping for a method wasn't found. this fixes it.

--- src/main/java/net/fabricmc/tinyremapper/extension/mixin/common/data/Message.java
+++ src/main/java/net/fabricmc/tinyremapper/extension/mixin/common/data/Message.java
@@ -22,7 +22,7 @@ public final class Message {
 	public static final String CANNOT_RESOLVE_CLASS = "Cannot resolve class %s";
 	public static final String CONFLICT_MAPPING = "Conflict mapping detected, %s -> %s.";
 	public static final String MULTIPLE_MAPPING_CHOICES = "Multiple conflicting mapping choices found for %s, which can be remapped to %s or %s. Such issues can be resolved by using fully qualified selectors.";
-	public static final String NO_MAPPING_NON_RECURSIVE = "Cannot remap %s because it does not exists in any of the targets %s";
-	public static final String NO_MAPPING_RECURSIVE = "Cannot remap %s because it does not exists in any of the targets %s or their parents.";
+	public static final String NO_MAPPING_NON_RECURSIVE = "Cannot remap %s because it does not exist in any of the targets %s";
+	public static final String NO_MAPPING_RECURSIVE = "Cannot remap %s because it does not exist in any of the targets %s or their parents.";
 	public static final String NOT_FULLY_QUALIFIED = "%s is not fully qualified.";
 }
