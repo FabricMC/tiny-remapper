386211493ddab077890330033d51caf2da9a3132
ishland <ishlandmc@yeah.net>
2026-05-20 01:16:58 +0800

    change: extract resolveAndRemapMixinRegex as a method

--- src/main/java/net/fabricmc/tinyremapper/extension/mixin/soft/annotation/injection/CommonInjectionAnnotationVisitor.java
+++ src/main/java/net/fabricmc/tinyremapper/extension/mixin/soft/annotation/injection/CommonInjectionAnnotationVisitor.java
@@ -100,35 +100,14 @@ class CommonInjectionAnnotationVisitor extends AnnotationVisitor {
 
 					// ending slash -> regex target
 					if (string.endsWith("/")) {
-						RegexMatcher matcher;
+						List<String> resolved = resolveAndRemapMixinRegex(string);
 
-						try {
-							matcher = RegexMatcher.parse(string);
-						} catch (RegexMatcher.ParsingException e) {
-							data.getLogger().warn("Error parsing mixin regex", e);
+						if (resolved == null) {
 							super.visit(name, value);
-							return;
-						}
-
-						List<? extends TrMethod> matchedMethods = Objects.requireNonNull(targets).stream()
-								.map(data.resolver::resolveClass)
-								.filter(Optional::isPresent)
-								.map(Optional::get)
-								.flatMap(trClass -> trClass.getMethods().stream())
-								.filter(trMethod -> matcher.matches(trMethod.getOwner().getName(), trMethod.getName(), trMethod.getDesc()))
-								.sorted(
-										Comparator
-												.<TrMethod, String>comparing(trMethod -> trMethod.getOwner().getName())
-												.thenComparing(TrMember::getName)
-												.thenComparing(TrMember::getDesc)
-								)
-								.collect(Collectors.toList());
-
-						for (TrMethod matchedMethod : matchedMethods) {
-							String mappedOwner = data.mapper.mapName(matchedMethod.getOwner());
-							String mappedName = data.mapper.mapName(matchedMethod);
-							String mappedDesc = data.mapper.mapDesc(matchedMethod);
-							super.visit(name, String.format("L%s;%s%s", mappedOwner, mappedName, mappedDesc));
+						} else {
+							for (String remappedSelector : resolved) {
+								super.visit(name, remappedSelector);
+							}
 						}
 
 						return;
@@ -191,6 +170,42 @@ class CommonInjectionAnnotationVisitor extends AnnotationVisitor {
 		}
 
 		return av;
+	}
+
+	private List<String> resolveAndRemapMixinRegex(String input) {
+		RegexMatcher matcher;
+
+		try {
+			matcher = RegexMatcher.parse(input);
+		} catch (RegexMatcher.ParsingException e) {
+			data.getLogger().warn("Error parsing mixin regex %s: %s", input, e.toString());
+			return null;
+		}
+
+		List<? extends TrMethod> matchedMethods = Objects.requireNonNull(targets).stream()
+				.map(data.resolver::resolveClass)
+				.filter(Optional::isPresent)
+				.map(Optional::get)
+				.flatMap(trClass -> trClass.getMethods().stream())
+				.filter(trMethod -> matcher.matches(trMethod.getOwner().getName(), trMethod.getName(), trMethod.getDesc()))
+				.sorted(
+						Comparator
+								.<TrMethod, String>comparing(trMethod -> trMethod.getOwner().getName())
+								.thenComparing(TrMember::getName)
+								.thenComparing(TrMember::getDesc)
+				)
+				.collect(Collectors.toList());
+
+		List<String> result = new ArrayList<>();
+
+		for (TrMethod matchedMethod : matchedMethods) {
+			String mappedOwner = data.mapper.mapName(matchedMethod.getOwner());
+			String mappedName = data.mapper.mapName(matchedMethod);
+			String mappedDesc = data.mapper.mapDesc(matchedMethod);
+			result.add(String.format("L%s;%s%s", mappedOwner, mappedName, mappedDesc));
+		}
+
+		return result;
 	}
 
 	private static class InjectMethodMappable implements IMappable<List<MemberInfo>> {
